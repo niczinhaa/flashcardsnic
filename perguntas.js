2aa500a650d68beed20550a53b8eb0f25f93a4f5
niczinhaa <160731528+niczinhaa@users.noreply.github.com>
2024-12-02 19:34:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual é o maior oceano do mundo?',
+    'Oceano Pacífico.'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quem foi o autor de Dom Quixote?',
+    'Miguel de Cervantes.'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual é a capital do Canadá?',
+    'Ottawa.'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Em que ano ocorreu a chegada do homem à Lua?',
+    '1969'
+)
